f037227dbb232da9e5c6e1bfed68664f2f20c372
Nick De Rees <r0841775@ucll.be>
2024-12-08 14:03:57 +0100

    no console message implemented

--- front-end/components/consoles/ConsoleOverview.tsx
+++ front-end/components/consoles/ConsoleOverview.tsx
@@ -24,26 +24,32 @@ const ConsoleOverview: React.FC<Props> = ({ consoles, onDeleteConsole }: Props) 
             </tr>
           </thead>
           <tbody>
-            {consoles.map((console, index) => (
-              <tr key={index}>
-                <td>{console.id}</td>
-                <td>{console.price}</td>
-                <td>{console.name}</td>
-                <td>{console.version}</td>
-                <td>{console.brand}</td>
-                <td>
-                  {console.releaseDate.substring(8, 10)}
-                  {console.releaseDate.substring(4, 8)}
-                  {console.releaseDate.substring(0, 4)}
+            {consoles.length > 0 ? (
+              consoles.map((console, index) => (
+                <tr key={index}>
+                  <td>{console.id}</td>
+                  <td>{console.price}</td>
+                  <td>{console.name}</td>
+                  <td>{console.version}</td>
+                  <td>{console.brand}</td>
+                  <td>
+                    {console.releaseDate.substring(8, 10)}
+                    {console.releaseDate.substring(4, 8)}
+                    {console.releaseDate.substring(0, 4)}
                   
-                </td>
-                <td
-                    onClick={() => onDeleteConsole(console)}
-                  >
-                    <p>Delete</p>
                   </td>
-              </tr>
-            ))}
+                  <td
+                      onClick={() => onDeleteConsole(console)}
+                    >
+                      <p>Delete</p>
+                    </td>
+                </tr>
+            ))
+          ) : (
+            <tr>
+              <td colSpan={7}>You currently don't have consoles</td>
+            </tr>
+          )}
           </tbody>
         </table>
       )}
