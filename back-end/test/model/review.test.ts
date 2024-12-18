41f9cdd6863d525d7dbfefe35a51ab0b5bae54e1
Nick De Rees <r0841775@ucll.be>
2024-12-18 14:01:47 +0100

    review model test finnished?

--- back-end/test/model/review.test.ts
+++ back-end/test/model/review.test.ts
@@ -2,22 +2,97 @@ import { Game } from '../../model/game';
 import { set } from 'date-fns';
 import { Review } from '../../model/review';
 
-const validGame = new Game({id: 1, name: "game1", genre: "horror", releaseDate: new Date(2021, 11, 17), developer: "Sony", consoles: []});
+const validGame = new Game({id: 1, name: "game1", genre: "horror", releaseDate: new Date(2021, 11, 17), developer: "Sony"});
 
 const validStars = 5;
 const validDescription = "Bad";
+const validReviewerId = 1;
 
 test('given: valid values for review, when: review is created, then: review is created with those values', () => {
 
     const review = new Review({
         id: 1,   
-        description: validDescription, 
         stars: validStars, 
+        description: validDescription, 
         game: validGame,
+        reviewerId: validReviewerId,
     });
     
     expect(review.getId()).toEqual(1);
     expect(review.getDescription()).toEqual(validDescription);
     expect(review.getStars()).toEqual(validStars);
     expect(review.getGame()).toEqual(validGame);
+    expect(review.getReviewerId()).toEqual(validReviewerId);
+});
+
+test('given: stars is invalid, when: console is created, then: an error is thrown', () => {
+    // given
+    const invalidStars = 0;
+
+    // when
+    const review = () =>
+        new Review({
+            id: 1,   
+            stars: invalidStars, 
+            description: validDescription, 
+            game: validGame,
+            reviewerId: validReviewerId,
+        });
+
+    // then
+    expect(review).toThrow('Stars is required');
+});
+
+test('given: description is invalid, when: console is created, then: an error is thrown', () => {
+    // given
+    const invalidDescription = '';
+
+    // when
+    const review = () =>
+        new Review({
+            id: 1,   
+            stars: validStars, 
+            description: invalidDescription, 
+            game: validGame,
+            reviewerId: validReviewerId,
+        });
+
+    // then
+    expect(review).toThrow('Description is required');
+});
+
+// test('given: game is invalid, when: console is created, then: an error is thrown', () => {
+//     // given
+//     const invalidGame = undefined;
+
+//     // when
+//     const review = () =>
+//         new Review({
+//             id: 1,   
+//             stars: validStars, 
+//             description: validDescription, 
+//             game: invalidGame,
+//             reviewerId: validReviewerId,
+//         });
+
+//     // then
+//     expect(review).toThrow('Game is required');
+// });
+
+test('given: reviewer id is invalid, when: console is created, then: an error is thrown', () => {
+    // given
+    const invalidReviewerId = 0;
+
+    // when
+    const review = () =>
+        new Review({
+            id: 1,   
+            stars: validStars, 
+            description: validDescription, 
+            game: validGame,
+            reviewerId: invalidReviewerId,
+        });
+
+    // then
+    expect(review).toThrow('Reviewer id is required');
 });
