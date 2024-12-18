4906107aca349354e11a793976fe2f72fe114fba
Nick De Rees <r0841775@ucll.be>
2024-12-18 13:50:50 +0100

    game model tests finnished? (release date skipped)

--- back-end/test/model/game.test.ts
+++ back-end/test/model/game.test.ts
@@ -1,25 +1,20 @@
-import { Console } from '../../model/console';
 import { Game } from '../../model/game';
 import { set } from 'date-fns';
 
-const validConsole = new Console({id: 1, price: 500, name: "Playstation", version: "4 pro", releaseDate: new Date(2021, 11, 17), brand: "Sony", games: []});
-
-
+const validId = 1;
 const validName = "game1";
 const validGenre = "horror";
 const validReleasDate = new Date(2021, 11, 17);
 const validDeveloper = "Sony";
-const validConsoles = [validConsole];
 
 test('given: valid values for game, when: game is created, then: game is created with those values', () => {
 
     const game = new Game({
-        id: 1, 
+        id: validId, 
         name: validName, 
         genre: validGenre,
         releaseDate: validReleasDate, 
         developer: validDeveloper, 
-        consoles: validConsoles,
     });
     
     expect(game.getId()).toEqual(1);
@@ -27,5 +22,58 @@ test('given: valid values for game, when: game is created, then: game is created
     expect(game.getGenre()).toEqual(validGenre);
     expect(game.getReleaseDate()).toEqual(validReleasDate);
     expect(game.getDeveloper()).toEqual(validDeveloper);
-    expect(game.getConsoles()).toContain(validConsole);
+});
+
+test('given: name is invalid, when: console is created, then: an error is thrown', () => {
+    // given
+    const invalidName = '';
+
+    // when
+    const game = () =>
+            new Game({
+            id: validId,  
+            name: invalidName, 
+            genre: validGenre,
+            releaseDate: validReleasDate, 
+            developer: validDeveloper,
+        });
+
+    // then
+    expect(game).toThrow('Name is required');
+});
+
+test('given: genre is invalid, when: console is created, then: an error is thrown', () => {
+    // given
+    const invalidGenre = '';
+
+    // when
+    const game = () =>
+            new Game({
+            id: validId,  
+            name: validName, 
+            genre: invalidGenre,
+            releaseDate: validReleasDate, 
+            developer: validDeveloper,
+        });
+
+    // then
+    expect(game).toThrow('Genre is required');
+});
+
+test('given: developer is invalid, when: console is created, then: an error is thrown', () => {
+    // given
+    const invalidDeveloper = '';
+
+    // when
+    const game = () =>
+            new Game({
+            id: validId,  
+            name: validName, 
+            genre: validGenre,
+            releaseDate: validReleasDate, 
+            developer: invalidDeveloper,
+        });
+
+    // then
+    expect(game).toThrow('Developer is required');
 });
