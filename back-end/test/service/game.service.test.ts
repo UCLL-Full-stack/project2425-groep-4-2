f8de7987533d76349b09cebe03a8cff640c4d1a9
Nick De Rees <r0841775@ucll.be>
2024-12-19 07:59:24 +0100

    added game service test i missed

--- back-end/test/service/game.service.test.ts
+++ back-end/test/service/game.service.test.ts
@@ -3,17 +3,48 @@ import gameDb from '../../repository/game.db';
 import gameService from '../../service/game.service';
 import { GameInput } from '../../types';
 
-let mock
+let mockGameDbGetAllGames: jest.Mock;
 let mockGameDbCreateGame: jest.Mock;
 
 beforeEach(() => {
+    mockGameDbGetAllGames = jest.fn();
     mockGameDbCreateGame = jest.fn();
-
+    
+    gameDb.getAllGames = mockGameDbGetAllGames;
     gameDb.createGame = mockGameDbCreateGame;
 });
 
 afterEach(() => {
     jest.clearAllMocks();
+});
+
+test('given valid games, when getting all games, then games are returned', async () => {
+            // given
+            const games = [
+                new Game({
+                    id: 1,
+                    name: 'Game 1',
+                    genre: 'RPG',
+                    releaseDate: new Date('2022-01-01'),
+                    developer: 'Phony'
+                }),
+                new Game({
+                    id: 2,
+                    name: 'Game 2',
+                    genre: 'Platformer',
+                    releaseDate: new Date('2023-01-01'),
+                    developer: "Macrohard"
+                }),
+            ];
+
+            mockGameDbGetAllGames.mockResolvedValue(games);
+
+            // when
+            const result = await gameService.getAllGames();
+
+            // then
+            expect(result).toEqual(games);
+            expect(mockGameDbGetAllGames).toHaveBeenCalledTimes(1);
 });
 
 test('given a valid game, when creating a game, then game is created', async () => {
